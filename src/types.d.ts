49a32e435e76be7d626eae7255c2f95fa7f088a0
Douglas Campbell <dacampbelltbone@gmail.com>
2020-12-12 03:09:40 -0500

    Init commit.  Add actions, reducer, store, created color selector.

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export type RootState = {
+  currentStroke: Stroke
+  strokes: Stroke[]
+}
+export type Stroke = {
+  points: Point[]
+  color: string
+}
+
+export type Point = {
+  x: number
+  y: number
+}
